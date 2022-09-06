5c16cd431a92a037fdbf1923152f7626393fc285
gegeeem <gegeeem@gmail.com>
2022-09-06 22:15:41 +0200

    addin state with useReducer

--- React.js/counter-use-reduce/src/components/Counter/Counter.js
+++ React.js/counter-use-reduce/src/components/Counter/Counter.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const inititalState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return inititalState;
+    default:
+      return state;
+  }
+};
 
 export default function Counter() {
-  return <div>
-    <button>Increment</button>
-    <button>Decrement</button>
-    <button>Reset</button>
-  </div>;
+  const [count, dispatch] = useReducer(reducer, inititalState);
+  return (
+    <div>
+      <div>Counter {count}</div>
+      <button
+        onClick={() => {
+          dispatch("increment");
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch("decrement");
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch("reset");
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
