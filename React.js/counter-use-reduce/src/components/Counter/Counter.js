4f81450679be083a53013a1501e5e9ec40e0c46f
gegeeem <gegeeem@gmail.com>
2022-09-06 21:28:17 +0200

    counter app structure

---
+++ React.js/counter-use-reduce/src/components/Counter/Counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Counter() {
+  return <div>
+    <button>Increment</button>
+    <button>Decrement</button>
+    <button>Reset</button>
+  </div>;
+}
