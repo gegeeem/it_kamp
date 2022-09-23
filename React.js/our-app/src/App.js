f79e10a986597258b00d86a00772619b4a1e9810
gegeeem <gegeeem@gmail.com>
2022-09-23 23:18:19 +0200

    adding pages

--- React.js/our-app/src/App.js
+++ React.js/our-app/src/App.js
@@ -1,19 +1,57 @@
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Card from "./components/Card/Card";
+import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
-      <div className="flex justify-center h-14  bg-plava mt-16 mx-10 rounded-xl px-8">
-        <div className="prvi">Prvi</div>
-        <div className="drugi">Drugi</div>
-        <div className="treci">treci</div>
-        <button className="bg-indigo-500">Button</button>
+    <>
+      {/* <div className="App">
+        <h1 className="text-4xl font-bold bg-red-100">Hello world!</h1>
       </div>
 
-      <div className="prvi"></div>
-    </div>
+      <div className="flex justify-between flex-row items-center w-1/2 mx-auto h-24 bg-orange-500 mt-20 rounded-xl px-8">
+        <div className="flex justify-center items-center bg-blue-500 w-16 h-16 text-white font-bold underline rounded-lg">
+          1
+        </div>
+        <div className="flex justify-center items-center bg-red-500 w-16 h-16 text-white font-bold underline rounded-full">
+          2
+        </div>
+        <div className="flex justify-center items-center bg-green-500 w-16 h-16 text-white font-bold underline rounded-2xl">
+          3
+        </div>
+      </div> */}
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        {/* <Route to={"/about"} element={<About />}></Route>
+        <Route to={"/products"} element={<Products />}></Route>
+        <Route to={"/cart"} element={<Cart />}></Route> */}
+      </Routes>
+      <div className="flex justify-center items-center">
+        <Card
+          price="30"
+          text="Replay muška majica kratkih rukava. Okrugli izrez. Pamuk. Teget boja."
+          images={
+            "https://www.n-sport.net/UserFiles/products/big/09/12/muska-majica-replay-316522832P-098.jpg"
+          }
+          title={"Replay Maica"}
+        />
+        <Card
+          price="40"
+          text="Replay muška majica kratkih rukava. Okrugli izrez. Pamuk. Teget boja."
+          images={
+            "https://img.ep-cdn.com/images/500/500/kg/kgkupgqfiytzjsnldglr.jpg"
+          }
+          title={"Replay Maica"}
+        />
+      </div>
+    </>
   );
 }
 
 export default App;
+
+// karktica slika  naslov cena
--- React.js/our-app/tailwind.config.js
+++ React.js/our-app/tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     // colors: {
     //   plava: "#4287f5",
     // },
+    customForms: (theme) => ({
+      default: {
+        checkbox: {
+          icon: '<svg fill="#1a202c" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" ><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>',
+          "&:hover": {
+            icon: '<svg fill="#4a5568" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" ><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>',
+          },
+        },
+      },
+    }),
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
